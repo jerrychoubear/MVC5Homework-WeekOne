2c7991a66b17d3885e544222808f921f94a75ea2
JJ <jerrychoubear@gmail.com>
2017-05-11 01:04:57 +0800

    加入專案檔案。

---
+++ MVC5Homework-WeekOne/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5Homework_WeekOne.MvcApplication" Language="C#" %>
---
+++ MVC5Homework-WeekOne/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Homework_WeekOne.Startup))]
+namespace MVC5Homework_WeekOne
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
